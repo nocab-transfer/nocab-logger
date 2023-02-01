142356258f9bbb8274c9def3e81b274c4cf13145
Berke Emin Kabagöz <berkekbgz@gmail.com>
2023-02-01 17:08:41 +0300

    Refactor: Remove logging package

--- lib/src/logger.dart
+++ lib/src/logger.dart
@@ -1,13 +1,13 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:intl/intl.dart';
 import 'package:nocab_logger/src/models/log.dart';
-import 'package:logging/logging.dart' as logging;
+import 'package:nocab_logger/src/models/log_level.dart';
 import 'package:path/path.dart';
 
 class Logger {
   final String name;
-  late logging.Logger _mainLogger;
   late File _file;
 
   final bool _isClosed = false;
@@ -15,44 +15,53 @@ class Logger {
 
   File get file => _file;
 
+  final StreamController<Log> _controller = StreamController<Log>.broadcast();
+  Stream<Log> get onLogged => _controller.stream;
+
   IOSink? _sink;
   Logger(this.name, {bool storeInFile = false, String? logPath, bool printLog = true}) : assert(!storeInFile || logPath != null) {
+    if (name.endsWith('.') || name.startsWith('.')) throw FormatException('Logger name cannot start or end with a dot (.)');
+    if (name.contains(':')) throw FormatException('Logger name cannot contain a colon (:)');
+    if (name.length < 2) throw FormatException('Logger name must be at least 2 characters long');
+
     if (storeInFile) {
       _file = File(join(logPath!, '$name-${DateFormat('yyyy-MM-dd').format(DateTime.now())}.log'))..createSync(recursive: true);
       _sink = _file.openWrite(mode: FileMode.append);
     }
 
-    _mainLogger = logging.Logger.detached(name);
-    _mainLogger.onRecord.listen((event) {
-      final log = Log.fromLogRecord(event);
-      if (printLog) print(log.toString().replaceAll('\\n', '\n'));
-      _sink?.writeln(log);
+    onLogged.listen((log) {
+      if (printLog) print(log.toString());
+      _sink?.writeln(log.toString().replaceAll('\n', '\\n'));
     });
   }
 
-  void info(String message, {Object? error, StackTrace? stackTrace}) {
+  void _log(LogLevel level, String message, {String? className, Object? error, StackTrace? stackTrace}) {
+    if (className != null && className.length < 2) throw FormatException('Class name must be at least 2 characters long');
     if (_isClosed) throw Exception('Logger is closed');
-    _mainLogger.info(message, error, stackTrace);
+    _controller.add(Log(level, message, name, className: className, error: error, stackTrace: stackTrace));
   }
 
-  void warning(String message, {Object? error, StackTrace? stackTrace}) {
-    if (_isClosed) throw Exception('Logger is closed');
-    _mainLogger.warning(message, error, stackTrace);
+  void info(String message, {String? className, Object? error, StackTrace? stackTrace}) {
+    _log(LogLevel.INFO, message, className: className, error: error, stackTrace: stackTrace);
   }
 
-  void error(String message, {Object? error, StackTrace? stackTrace}) {
-    if (_isClosed) throw Exception('Logger is closed');
-    _mainLogger.severe(message, error, stackTrace);
+  void warning(String message, {String? className, Object? error, StackTrace? stackTrace}) {
+    _log(LogLevel.WARNING, message, className: className, error: error, stackTrace: stackTrace);
   }
 
-  void fatal(String message, {Object? error, StackTrace? stackTrace}) {
-    if (_isClosed) throw Exception('Logger is closed');
-    _mainLogger.shout(message, error, stackTrace);
+  void error(String message, {String? className, Object? error, StackTrace? stackTrace}) {
+    _log(LogLevel.ERROR, message, className: className, error: error, stackTrace: stackTrace);
   }
 
-  Future<void> close() async {
+  void fatal(String message, {String? className, Object? error, StackTrace? stackTrace}) {
+    _log(LogLevel.FATAL, message, className: className, error: error, stackTrace: stackTrace);
+  }
+
+  Future<void> close({bool deleteFile = false}) async {
+    await _controller.close();
     await _sink?.flush();
     await _sink?.close();
+    if (deleteFile) await _file.delete();
   }
 
   static Future<bool> isFileValid(File file) async {
--- lib/src/models/log.dart
+++ lib/src/models/log.dart
@@ -1,30 +1,24 @@
-import 'package:logging/logging.dart';
-import 'package:nocab_logger/src/extensions/level_converter.dart';
 import 'package:nocab_logger/src/models/log_level.dart';
 
 class Log {
   final LogLevel level;
   final String message;
   final String loggerName;
+  final String? className;
   final Object? error;
   final StackTrace? stackTrace;
   late DateTime time;
 
-  Log(this.level, this.message, this.loggerName, {this.error, this.stackTrace, DateTime? time}) : time = time ?? DateTime.now();
-
-  factory Log.fromLogRecord(LogRecord record) {
-    return Log(record.level.logLevel, record.message, record.loggerName, error: record.error, stackTrace: record.stackTrace, time: record.time);
-  }
+  Log(this.level, this.message, this.loggerName, {this.className, this.error, this.stackTrace, DateTime? time}) : time = time ?? DateTime.now();
 
   // Level, Time, Classname, Message are required. Error and StackTrace are optional
   static final String _regex = r'\[(?<level>'
       '${LogLevel.values.map((e) => e.name).join('|')}' // get all the log levels dynamically
-      r')\] - (?<time>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{0,}) (?<classname>.*?): (?<message>[\w\W][^\[]+) ?(?:\[(?<error>[\w\W][^\[]+)\])? ?(?:\[StackTrace: (?<stackTrace>[\w\W][^\]]+)?\])?';
+      r')\] - (?<time>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{0,}) (?<loggerName>\S[^\.\:]+)(?:\.(?<classname>\S[^\:]+))?: (?<message>\S[^\[]+) ?(?:\[(?<error>\S[^\[]+)\])? ?(?:\[StackTrace: (?<stackTrace>\S[^\]]+)?\])?';
 
   @override
   String toString() {
-    return '[${level.name}] - ${time.toIso8601String()} $loggerName: $message ${error != null ? '[$error]' : ''} ${stackTrace != null ? '[StackTrace: ${stackTrace.toString().trim()}]' : ''}'
-        .replaceAll('\n', '\\n');
+    return '[${level.name}] - ${time.toIso8601String()} $loggerName${className != null ? '.$className' : ''}: $message ${error != null ? '[$error]' : ''} ${stackTrace != null ? '[StackTrace: ${stackTrace.toString().trim()}]' : ''}';
   }
 
   static bool checkValidity(String logLine) {
@@ -39,11 +33,13 @@ class Log {
 
     String level = match.namedGroup('level')!;
     DateTime time = DateTime.parse(match.namedGroup('time')!);
-    String classname = match.namedGroup('classname')!.replaceAll('\\n', '\n');
+    String loggerName = match.namedGroup('loggerName')!.replaceAll('\\n', '\n');
+    String? classname = match.namedGroup('classname')?.replaceAll('\\n', '\n');
     String message = match.namedGroup('message')!.replaceAll('\\n', '\n');
     Object? error = match.namedGroup('error')?.replaceAll('\\n', '\n');
     StackTrace? stackTrace =
         match.namedGroup('stackTrace') != null ? StackTrace.fromString(match.namedGroup('stackTrace')!.replaceAll('\\n', '\n')) : null;
-    return Log(LogLevel.values.firstWhere((e) => e.name == level), message, classname, error: error, stackTrace: stackTrace, time: time);
+    return Log(LogLevel.values.firstWhere((e) => e.name == level), message, loggerName,
+        className: classname, error: error, stackTrace: stackTrace, time: time);
   }
 }
