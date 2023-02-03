6d43e58715e11c77322a6c0cb57f57fb2cda0553
Berke Emin Kabagöz <berkekbgz@gmail.com>
2023-02-03 15:45:59 +0300

    Feat: Remove unused logging package

--- lib/src/models/log_level.dart
+++ lib/src/models/log_level.dart
@@ -1,13 +1,8 @@
 // ignore_for_file: constant_identifier_names
 
-import 'package:logging/logging.dart';
-
 enum LogLevel {
-  INFO(Level.INFO),
-  WARNING(Level.WARNING),
-  ERROR(Level.SEVERE),
-  FATAL(Level.SHOUT);
-
-  final Level loggingLevel;
-  const LogLevel(this.loggingLevel);
+  INFO,
+  WARNING,
+  ERROR,
+  FATAL;
 }
